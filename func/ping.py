345df39e0ea3eeacec9bf37b9ed217bccf2778dc
lcolok <425311101@qq.com>
2022-12-10 23:22:27 +0800

    Greatly reduced the time required for ping

--- func/ping.py
+++ func/ping.py
@@ -2,51 +2,36 @@ region='未知'
 ip=''
 port=''
     
+import threading
+import os
+import time
+
 def ping_threading(ipDict):
-    import threading
-    import subprocess
-    import time
-    from queue import Queue
-
-    # 定义工作线程
-    WORD_THREAD = 10
-
-    # 将需要 ping 的 ip 加入队列
-    IP_QUEUE = Queue() 
-    for i in ipDict:
-        IP_QUEUE.put({'ip':i['ip'],'region':i['region'],'port':i['port']})
-
-    # 定义一个执行 ping 的函数
-
-
-    def ping_ip():
-        while (not IP_QUEUE.empty()):
-            cb = IP_QUEUE.get()
-            # print(cb)
-            test_ip = cb['ip']
-            test_region = cb['region']
-            test_port = cb['port']
-            res = subprocess.call('ping -c 1 -w 1 %s' % test_ip,stdout=subprocess.PIPE,shell=True)  # linux 系统将 '-n' 替换成 '-c' ，而且设置shell=True，详情请参考：https://xilou.info/p/118
-            # 打印运行结果
-            if res == 0:
-                # 引入全局变量：https://blog.csdn.net/songyunli1111/article/details/76095971
-                global region,ip,port 
-                region = test_region
-                ip = test_ip
-                port = test_port
-                print(region,ip,port)
-                break
-        
-    threads = []
     start_time = time.time()
-    for i in range(WORD_THREAD):
-        thread = threading.Thread(target=ping_ip)
+    # 创建一个事件，用于在成功 ping 通某个 IP 地址后通知其他线程退出
+    event = threading.Event()
+
+    # 存储成功 ping 通的 IP 地址
+    result = []
+
+    # 为每个 IP 地址创建一个线程，用于 ping 该地址
+    for item in ipDict:
+        thread = threading.Thread(target=ping_ip, args=(item, result, event))
         thread.start()
-        threads.append(thread)
 
-    for thread in threads:
-        thread.join()
-    
-    print('Ping程序运行耗时：%s' % (time.time() - start_time))
-    return {'region':region,'ip':ip,'port':port}
+    # 如果有线程成功 ping 通了某个 IP 地址，则通知其他线程退出
+    if event.wait():
+        print('Ping程序运行耗时：%s' % (time.time() - start_time))
+        # 返回成功 ping 通的 IP 地址
+        return result[0]
 
+def ping_ip(item, result, event):
+    # 从字典中获取 IP 地址
+    ip = item['ip']
+
+    # 使用 Linux 的 ping 命令测试该 IP 地址是否可以 ping 通
+    if os.system('ping -c 1 -w 1 ' + ip) == 0:
+        # 如果可以 ping 通，则将该地址添加到结果列表中
+        result.append(item)
+        # 通知其他线程退出
+        event.set()
