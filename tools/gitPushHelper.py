483b33323488bcf3772175889c7b05b5c39ca3d0
lcolok <425311101@qq.com>
2022-12-17 23:54:19 +0800

    add color

--- tools/gitPushHelper.py
+++ tools/gitPushHelper.py
@@ -14,7 +14,7 @@ def print_with_color(string, keywords):
     print("\033[1;32m", end="")
 
     # 遍历字符串中的每一个字眼
-    for word in cmd.split(" "):
+    for word in string.split(" "):
         # 如果这个字眼是关键词，就将它的颜色更改为相应的颜色
         if word in keywords:
             print(keywords[word], end="")
@@ -27,18 +27,18 @@ def print_with_color(string, keywords):
     print("\033[0m")
 
 keywords = {
-    "&&": "\033[0m",
-    "git": "\033[1;31m",
-    "user": "\033[1;32m",
-    "config": "\033[1;33m",
-    "--global": "\033[1;34m",
-    "http.proxy": "\033[1;35m",
-    "user.email": "\033[1;36m",
-    "user.name": "\033[1;37m",
-    "http.proxy": "\033[1;38m",
-    "proxyURL": "\033[1;39m",
-    "cd": "\033[1;31m",
-    "dir_path": "\033[1;41m",
+    "&&": "\033[0m",# &&：正常颜色
+    "git": "\033[1;31m",# git：红色
+    "user": "\033[1;32m",# user：绿色
+    "config": "\033[1;33m",# config：黄色
+    "--global": "\033[1;34m",# --global：蓝色
+    "http.proxy": "\033[1;35m",# http.proxy：紫色
+    "user.email": "\033[1;36m",# user.email：深蓝色
+    "user.name": "\033[1;37m",# user.name：深绿色
+    "http.proxy": "\033[1;38m",# http.proxy：深紫色
+    "proxyURL": "\033[1;39m",# proxyURL：深红色
+    "cd": "\033[1;31m",# cd：红色
+    "dir_path": "\033[1;41m",# dir_path：深黑色
 }
 
 cmd = f'git config --global http.proxy {proxyURL} && git config --global user.email 425311101@qq.com && git config --global user.name lcolok && cd {dir_path} && git push'
