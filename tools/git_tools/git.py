e32b2d3c9e516d048871c36b94412f1641de8be1
lcolok <425311101@qq.com>
2023-02-19 16:29:03 +0800

    enhance models mount

--- tools/git_tools/git.py
+++ tools/git_tools/git.py
@@ -1,7 +1,9 @@
 import os
 
+search_dir = os.path.join(os.getcwd(), "OneClick-stable-diffusion")
+
 # find all subdirectories
-for root, dirs, files in os.walk(os.getcwd()):
+for root, dirs, files in os.walk(search_dir):
   # check if the file exists in the current directory
   if "gitPushHelper.py" in files:
     # build the path to the file
