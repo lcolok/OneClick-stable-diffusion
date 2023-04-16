6c5b600a07931b1d003867ace146622fd5d3db07
lcolok <425311101@qq.com>
2023-04-17 01:38:30 +0800

    update git.py

--- tools/git_tools/git.py
+++ tools/git_tools/git.py
@@ -1,12 +1,23 @@
 import os
 
-search_dir = os.path.join(os.getcwd(), "OneClick-stable-diffusion")
+# 获取包含当前脚本的目录
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
-# find all subdirectories
-for root, dirs, files in os.walk(search_dir):
-  # check if the file exists in the current directory
-  if "gitPushHelper.py" in files:
-    # build the path to the file
-    path = os.path.join(root, "gitPushHelper.py")
-    # run the file
+# 在当前目录中查找 gitPushHelper.py 文件
+if "gitPushHelper.py" in os.listdir(base_dir):
+    # 构建 gitPushHelper.py 文件的路径
+    path = os.path.join(base_dir, "gitPushHelper.py")
+    # 执行 gitPushHelper.py 文件
     os.system(f"python {path}")
+else:
+    # 在指定目录及其子目录中递归查找 gitPushHelper.py 文件
+    search_dir = os.path.join(os.getcwd(), "OneClick-stable-diffusion")
+    for root, dirs, files in os.walk(search_dir):
+        if "gitPushHelper.py" in files:
+            # 构建 gitPushHelper.py 文件的路径
+            path = os.path.join(root, "gitPushHelper.py")
+            # 执行 gitPushHelper.py 文件
+            os.system(f"python {path}")
+            break  # 在找到第一个 gitPushHelper.py 文件后停止搜索
+    else:  # 如果 for 循环没有遇到 break
+        print("未找到 gitPushHelper.py 文件。")
