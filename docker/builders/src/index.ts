b35be99b9b1c38c9669b0a79ef4671a56d6e1624
lcolok <425311101@qq.com>
2023-04-19 16:02:27 +0800

    clean code

--- docker/builders/src/index.ts
+++ docker/builders/src/index.ts
@@ -10,7 +10,7 @@ import {
   text,
 } from "@clack/prompts";
 import { setTimeout as sleep } from "node:timers/promises";
-import color from "picocolors";
+import pc from "picocolors";
 import { promisify } from "util";
 import { exec as execCallback } from "child_process";
 const exec = promisify(execCallback);
@@ -33,7 +33,7 @@ async function buildImage(tag: string, dockerfile: string): Promise<void> {
     });
 
     buildProcess.stderr.on("data", (data) => {
-      console.error(color.green(data.toString()));
+      console.error(pc.green(data.toString()));
     });
 
     buildProcess.on("exit", (code) => {
@@ -46,16 +46,9 @@ async function buildImage(tag: string, dockerfile: string): Promise<void> {
   });
 }
 
-type BuildConfigType = {
-  [key: string]: {
-    tag: string;
-    dockerfile: string;
-  };
-};
-
 async function main(): Promise<void> {
   console.log();
-  intro(color.inverse(" 简易构建容器镜像 "));
+  intro(pc.inverse(" 简易构建容器镜像 "));
 
   const projectType: string | symbol | null = await select({
     message: "选择一个构建方案：",
@@ -87,6 +80,32 @@ async function main(): Promise<void> {
     return process.exit(0);
   }
 
+  type BuildConfigType = {
+    [key: string]: {
+      tag: string;
+      dockerfile: string;
+      dependencies?: string[];
+    };
+  };
+
+  async function printDockerImages(
+    selectedConfig: BuildConfigType[keyof BuildConfigType]
+  ) {
+    const { stdout, stderr } = await exec("docker images");
+    console.log();
+    console.log(pc.bold("所有 Docker 镜像列表："));
+    const images = stdout.split("\n").slice(1).filter(Boolean);
+    images.forEach((image) => {
+      const [repository, tag, imageId, created, size] = image.split(/\s+/);
+      const name = `${repository}:${tag}`;
+      if (name === selectedConfig.tag) {
+        console.log(pc.bold(pc.cyan(image)));
+      } else {
+        console.log(image);
+      }
+    });
+  }
+
   const buildConfig: BuildConfigType = {
     conda_build: {
       tag: "conda:jupyter",
@@ -95,31 +114,52 @@ async function main(): Promise<void> {
     py_build: {
       tag: "py:3.10.6-torch2.0.0",
       dockerfile: "Dockerfile.py",
+      dependencies: ["conda_build"],
     },
     sdwebui_base_build: {
       tag: "sdwebui_base:rtx4090",
       dockerfile: "Dockerfile.sdwebui_base",
+      dependencies: ["conda_build", "py_build"],
     },
     sdwebui_ext_build: {
       tag: "sdwebui_ext:rtx4090",
       dockerfile: "Dockerfile.sdwebui_ext",
+      dependencies: ["conda_build", "py_build", "sdwebui_base_build"],
     },
   };
 
   const selectedConfig = buildConfig[projectType as string];
 
-  if (selectedConfig) {
+  async function buildImagesRecursively(projectType: string): Promise<void> {
+    const selectedConfig = buildConfig[projectType];
+    if (!selectedConfig) {
+      return;
+    }
+
+    // 如果有依赖项，则先构建依赖项
+    if (selectedConfig.dependencies) {
+      for (const dependency of selectedConfig.dependencies) {
+        await buildImagesRecursively(dependency);
+      }
+    }
+
+    // 构建当前方案
     const s = spinner();
     s.start(`通过Docker构建${selectedConfig.tag}镜像`);
     try {
       await buildImage(selectedConfig.tag, selectedConfig.dockerfile);
       s.stop(`${selectedConfig.tag}镜像已成功通过Docker构建`);
     } catch (error: any) {
-      // 修改这一行，使其包含 `: any`
-      console.error(color.red(error.message));
+      console.error(pc.red(error.message));
       cancel(`${selectedConfig.tag}镜像构建失败`);
       return process.exit(1);
     }
+  }
+
+  // 在 main 函数中调用 buildImagesRecursively 函数
+  if (selectedConfig) {
+    await buildImagesRecursively(projectType as string);
+    await printDockerImages(selectedConfig);
   }
 
   outro("You're all set!");
