029218ae61c06730960c7d0b1f7c388201c3ef5b
lcolok <425311101@qq.com>
2023-04-24 22:00:03 +0800

    simplified

--- docker/scripts/run_python.sh
+++ docker/scripts/run_python.sh
@@ -6,9 +6,9 @@ conda activate py3.10.6
 
 # 运行 Python 脚本
 cd $NOTEBOOK_DIR/stable-diffusion-webui
-# python webui.py \ # 快速启动,但是前提是已经安装了依赖,否则无法正常启动,需要进一步调试
-    # --nowebui \
-python launch.py \
+
+# 快速启动,但是前提是已经安装了依赖,否则无法正常启动,需要进一步调试
+python webui.py \
     --port ${SDWEBUI_PORT} \
     --api \
     --disable-safe-unpickle \
@@ -16,3 +16,4 @@ python launch.py \
     --listen \
     --skip-install \
     --ckpt ./models/Stable-diffusion/revAnimated_v11.safetensors
+    # --nowebui \
