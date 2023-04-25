92d692c72f712ad40f217846e1ed66b3c55f8f4a
lcolok <425311101@qq.com>
2023-04-26 02:23:57 +0800

    add checkAndInstallScreen

--- docker/src/modules/installAutoLauncher.ts
+++ docker/src/modules/installAutoLauncher.ts
@@ -2,59 +2,98 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { exec, execSync } from 'child_process';
 
-async function installAutoLauncher(): Promise<void> {
-  // 获取当前工作目录的绝对路径
-  const currentDirectory = path.resolve(path.dirname(''));
-  const tempDirectory = path.join(currentDirectory, 'temp');
+// 检查并安装 screen
+async function checkAndInstallScreen(): Promise<void> {
+    try {
+        execSync('screen --version', { stdio: 'ignore' });
+    } catch (error) {
+        console.log('未检测到 screen，现在将自动安装...');
+        try {
+            execSync('sudo apt-get update && sudo apt-get install -y screen', { stdio: 'inherit' });
+            console.log('已成功安装 screen。');
+        } catch (installError) {
+            console.error('安装 screen 时出错，请手动安装。');
+            throw installError;
+        }
+    }
+}
 
-  // 检查 temp 目录是否存在，如果不存在，则创建一个
-  if (!fs.existsSync(tempDirectory)) {
-    fs.mkdirSync(tempDirectory);
-  }
+// 创建临时目录
+function createTempDirectory(currentDirectory: string): string {
+    const tempDirectory = path.join(currentDirectory, 'temp');
+    if (!fs.existsSync(tempDirectory)) {
+        fs.mkdirSync(tempDirectory);
+    }
+    return tempDirectory;
+}
 
-  // 创建 autolaunch_sd.service 文件内容
-  const serviceFileContent = `[Unit]
-  Description=Auto launch container
-  After=docker.service
-  
-  [Service]
-  Restart=always
-  RemainAfterExit=yes
-  WorkingDirectory=${currentDirectory}
-  ExecStart=/usr/bin/screen -S sd -dm /usr/bin/python3 dash.py --name autolaunch --port_increment 1
-  ExecStop=/usr/bin/screen -S sd -X quit
-  
-  [Install]
-  WantedBy=multi-user.target
+// 创建并保存 systemd 服务文件
+function createAndSaveServiceFile(currentDirectory: string, tempDirectory: string): string {
+    const serviceFileContent = `[Unit]
+Description=Auto launch container
+After=docker.service
+
+[Service]
+Restart=always
+RemainAfterExit=yes
+WorkingDirectory=${currentDirectory}
+ExecStart=/usr/bin/screen -S sd -dm /usr/bin/python3 dash.py --name autolaunch --port_increment 1
+ExecStop=/usr/bin/screen -S sd -X quit
+
+[Install]
+WantedBy=multi-user.target
 `;
 
-  // 将内容写入 service 文件
-  const serviceFilePath = path.join(tempDirectory, 'autolaunch_sd.service');
-  fs.writeFileSync(serviceFilePath, serviceFileContent);
+    const serviceFilePath = path.join(tempDirectory, 'autolaunch_sd.service');
+    fs.writeFileSync(serviceFilePath, serviceFileContent);
+    return serviceFilePath;
+}
 
-  // 复制 service 文件到 /etc/systemd/system 目录（需要 root 权限）
-  execSync(`sudo cp ${serviceFilePath} /etc/systemd/system`);
+// 复制服务文件到 systemd 目录
+function copyServiceFileToSystemd(serviceFilePath: string) {
+    execSync(`sudo cp ${serviceFilePath} /etc/systemd/system`);
+}
 
-  // 输出提示信息
-  console.log("Service file 'autolaunch_sd.service' has been created in the 'temp' directory and copied to /etc/systemd/system.");
+// 启动服务并显示状态
+function startServiceAndShowStatus() {
+    execSync('sudo systemctl stop autolaunch_sd.service');
+    execSync('sudo systemctl daemon-reload');
+    execSync('sudo systemctl start autolaunch_sd.service');
 
-  // Reload systemd daemon and start the service
-  execSync('sudo systemctl stop autolaunch_sd.service');
-  execSync('sudo systemctl daemon-reload');
-  execSync('sudo systemctl start autolaunch_sd.service');
+    exec('sudo systemctl status autolaunch_sd.service', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`错误: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`标准错误: ${stderr}`);
+            return;
+        }
+        console.log(`标准输出: ${stdout}`);
+    });
+}
 
-  // Check the status of the service
-  exec('sudo systemctl status autolaunch_sd.service', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Stdout: ${stdout}`);
-  });
+async function installAutoLauncher(): Promise<void> {
+    // 检查并安装 screen
+    await checkAndInstallScreen();
+
+    // 获取当前工作目录的绝对路径
+    const currentDirectory = path.resolve(path.dirname(''));
+
+    // 创建临时目录
+    const tempDirectory = createTempDirectory(currentDirectory);
+
+    // 创建并保存 systemd 服务文件
+    const serviceFilePath = createAndSaveServiceFile(currentDirectory, tempDirectory);
+
+    // 输出提示信息
+    console.log("服务文件 'autolaunch_sd.service' 已在 'temp' 目录创建并复制到 /etc/systemd/system.");
+
+    // 复制服务文件到 systemd 目录
+    copyServiceFileToSystemd(serviceFilePath);
+
+    // 启动服务并显示状态
+    startServiceAndShowStatus();
 }
 
 export { installAutoLauncher };
