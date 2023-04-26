5e203e572fd4ce3ddfe7009289eb5f5d8e589b03
lcolok <425311101@qq.com>
2023-04-27 01:40:47 +0800

    improve i18n

--- docker/src/commands/install.ts
+++ docker/src/commands/install.ts
@@ -2,7 +2,5 @@ import { installAutoLauncher } from '../modules/installAutoLauncher';
 import i18next from '../i18n';
 
 export async function install(): Promise<void> {
-  installAutoLauncher()
-    .then(() => console.log(i18next.t("AUTO_LAUNCHER_INSTALLED_SUCCESSFULLY")))
-    .catch((error) => console.error(`${i18next.t("ERROR_INSTALLING_AUTO_LAUNCHER")}: ${error}`));
+  await installAutoLauncher();
 }
--- docker/src/modules/installAutoLauncher.ts
+++ docker/src/modules/installAutoLauncher.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { exec, execSync } from 'child_process';
+import { exec, execSync, spawn, SpawnOptions } from 'child_process';
 import i18next from '../i18n';
 
 // 检查并安装 screen
@@ -57,22 +57,32 @@ function copyServiceFileToSystemd(serviceFilePath: string) {
 
 // 启动服务并显示状态
 function startServiceAndShowStatus() {
+    const spawnOptions: SpawnOptions = {
+        stdio: ["pipe", "inherit", "inherit"],
+    };
+
+    // 输出停止、重新加载和启动服务的提示信息
+    console.log(i18next.t('STOP_RELOAD_START'));
+
+    // 停止并重新启动服务
     execSync('sudo systemctl stop autolaunch_sd.service');
     execSync('sudo systemctl daemon-reload');
     execSync('sudo systemctl start autolaunch_sd.service');
 
-    exec('sudo systemctl status autolaunch_sd.service', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`${i18next.t("ERROR")}: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`${i18next.t("STD_ERR")}: ${stderr}`);
-            return;
-        }
-        console.log(`${i18next.t("STD_OUT")}: ${stdout}`);
+    // 启动子进程并将标准输出和标准错误输出传递到父进程的输出中
+    const systemctl = spawn('sudo', ['systemctl', 'status', 'autolaunch_sd.service'], spawnOptions);
+
+    systemctl.on('close', (code: number) => {
+        console.log(i18next.t('CLOSED_WITH_CODE', { code }));
+        console.log(i18next.t("AUTO_LAUNCHER_INSTALLED_SUCCESSFULLY"))
+    });
+
+    systemctl.on('error', (err: Error) => {
+        console.error(i18next.t('ERROR_PRINT', { err }));
+        console.error(`${i18next.t("ERROR_INSTALLING_AUTO_LAUNCHER")}: ${err}`)
     });
 }
+
 
 async function installAutoLauncher(): Promise<void> {
     // 检查并安装 screen
@@ -88,7 +98,7 @@ async function installAutoLauncher(): Promise<void> {
     const serviceFilePath = createAndSaveServiceFile(currentDirectory, tempDirectory);
 
     // 输出提示信息
-    console.log(i18next.t("SERVICE_FILE_CREATED", {filename: "autolaunch_sd.service", directory: "temp"}));
+    console.log(i18next.t("SERVICE_FILE_CREATED", { filename: "autolaunch_sd.service", directory: "temp" }));
 
     // 复制服务文件到 systemd 目录
     copyServiceFileToSystemd(serviceFilePath);
