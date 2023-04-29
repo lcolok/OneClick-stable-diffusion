d8aed44d3df118e486abc2b0baf1ecd9888c97d5
lcolok <425311101@qq.com>
2023-04-30 01:16:37 +0800

    fix runCommand cannot stop by ctrl+C

--- docker/src/utils/runCommand.ts
+++ docker/src/utils/runCommand.ts
@@ -1,30 +1,50 @@
-import { spawn, SpawnOptions } from "child_process";
-import { outro, cancel } from "@clack/prompts";
-import pc from "picocolors";
+import { spawn, ChildProcess } from "child_process";
+import { cancel } from "@clack/prompts";
+import { Writable } from "stream";
 import i18next from '../i18n';
 
-export async function runCommand(command: string, args: string[], options?: SpawnOptions): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const childProcess = spawn(command, args, { stdio: "inherit", ...options });
+const childProcesses = new Set<ChildProcess>();
 
-        // Handle Ctrl+C (SIGINT) signal
-        process.on("SIGINT", () => {
-            console.log(`\n${pc.bold(i18next.t("TERMINATING_CHILD_PROCESS"))}`);
-            childProcess.kill();
-            cancel(i18next.t("CHILD_PROCESS_TERMINATED") as string);
-            process.exit(0);
+interface RunCommandOptions {
+    cwd?: string;
+}
+
+export async function runCommand(
+    command: string,
+    args: string[],
+    options?: RunCommandOptions
+): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const childProcess = spawn(command, args, {
+            stdio: ["pipe", "inherit", "inherit"],
+            cwd: options?.cwd,
         });
 
+        childProcesses.add(childProcess);
+
+        if (process.stdin.isTTY) {
+            process.stdin.setRawMode(true);
+            process.stdin.pipe(childProcess.stdin as Writable);
+            process.stdin.on("data", (data) => {
+                if (data.toString() === "\x03") {
+                    process.stdin.unpipe(childProcess.stdin as Writable);
+                    childProcess.stdin && childProcess.stdin.destroy();
+                    console.log(`\n${i18next.t("TERMINATING_CHILD_PROCESS")}`);
+                    childProcesses.forEach((cp) => cp.kill());
+                    cancel(i18next.t("CHILD_PROCESS_TERMINATED") as string);
+                    process.exit(0);
+                }
+            });
+        }
+
         childProcess.on("close", (code) => {
+            childProcesses.delete(childProcess);
+
             if (code === 0) {
                 resolve();
             } else {
                 reject(new Error(`${i18next.t("CHILD_PROCESS_EXITED")} ${code}`));
             }
-        });
-
-        childProcess.on("error", (error) => {
-            reject(error);
         });
     });
 }
