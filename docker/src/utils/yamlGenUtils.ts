f82ddce64f99d63e7c8005142e872b1bdbaf2c1d
lcolok <425311101@qq.com>
2023-05-01 00:22:53 +0800

    Add support for recursively creating output directory

--- docker/src/utils/yamlGenUtils.ts
+++ docker/src/utils/yamlGenUtils.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { DockerComposeConfig } from 'dockerComposeTypes';
 import * as yaml from 'js-yaml';
+import * as path from 'path';
 
 export function generateDockerComposeYaml(config: DockerComposeConfig): string {
     return JSON.stringify(config);
@@ -11,5 +12,9 @@ export function writeDockerComposeYamlToFile(
     outputPath: string
 ): void {
     const yamlContent = yaml.dump(JSON.parse(generateDockerComposeYaml(config)));
+    const outputDir = path.dirname(outputPath);
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true });
+    }
     fs.writeFileSync(outputPath, yamlContent, { encoding: 'utf-8' });
 }
