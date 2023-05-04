ca531dcf28dbed3efff327ea069181907342a456
lcolok <425311101@qq.com>
2023-05-05 00:38:01 +0800

    perfect run lama clean and plugins

--- docker/context/scripts/run_lama_cleaner.sh
+++ docker/context/scripts/run_lama_cleaner.sh
@@ -5,4 +5,10 @@ eval "$(conda shell.bash hook)"
 
 # 启动 LAMA Cleaner 服务器
 conda activate lama_cleaner && \
-lama-cleaner --model=lama --device=cuda --host=0.0.0.0 --port=${LAMA_CLEANER_PORT}
+lama-cleaner --model=lama --device=cuda --host=0.0.0.0 --port=${LAMA_CLEANER_PORT} \
+--enable-realesrgan --realesrgan-model=RealESRGAN_x4plus --realesrgan-device=cuda \
+--enable-gfpgan --gfpgan-device cuda \
+--enable-restoreformer --restoreformer-device cuda \
+--enable-remove-bg \
+--enable-interactive-seg --interactive-seg-model=vit_l  --interactive-seg-device=cuda \
+--enable-gif
