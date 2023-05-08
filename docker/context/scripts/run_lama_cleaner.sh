99ad423aa6a7b91eeba5b8002ac3dbbdf22333dc
lcolok <425311101@qq.com>
2023-05-09 01:14:59 +0800

    little improve

--- docker/context/scripts/run_lama_cleaner.sh
+++ docker/context/scripts/run_lama_cleaner.sh
@@ -7,8 +7,14 @@ eval "$(conda shell.bash hook)"
 conda activate py3.10.6 &&
     lama-cleaner --model=lama --device=cuda --host=0.0.0.0 --port=${LAMA_CLEANER_PORT} \
         --enable-realesrgan --realesrgan-model=RealESRGAN_x4plus --realesrgan-device=cuda \
-        --enable-gfpgan --gfpgan-device cuda \
-        --enable-restoreformer --restoreformer-device cuda \
         --enable-remove-bg \
-        --enable-interactive-seg --interactive-seg-model=vit_h --interactive-seg-device=cuda \
-        --enable-gif
+        --enable-interactive-seg --interactive-seg-model=vit_l --interactive-seg-device=cuda
+
+# --enable-gif
+
+# --enable-gfpgan --gfpgan-device cuda \
+# --enable-restoreformer --restoreformer-device cuda \
+
+# default or vit_h: ViT-H SAM model.
+# vit_l: ViT-L SAM model.
+# vit_b: ViT-B SAM model.
