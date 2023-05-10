9df56edae9d58f58c6d780365454de8812a37cee
lcolok <425311101@qq.com>
2023-05-10 20:59:20 +0800

    Optimize YAML parsing and variable substitution

--- docker/src/configs/readConfig.ts
+++ docker/src/configs/readConfig.ts
@@ -1,27 +1,31 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as path from 'path';
+import nunjucks from 'nunjucks';
 import { GlobalConfigTypes } from '@types';
 import { generateBuildConfigTypesWithDockerfilePath } from './imageBuildConfig';
 
-// 获取配置文件路径
 const configFile = path.join(__dirname, './globalConfig.yaml');
+const yamlContent = fs.readFileSync(configFile, 'utf8');
 
-// 读取配置文件并解析为 TypeScript 类型
-const parsedGlobalConfig = yaml.load(
-  fs.readFileSync(configFile, 'utf8'),
-) as GlobalConfigTypes;
+const parsedYaml = yaml.load(yamlContent) as any;
+const templateVariables = parsedYaml.templateVariables;
 
-// 从 dockerBuildConfig 中获取 endpointBuild 为 true 的项目，得知新的 buildList 列表
+delete parsedYaml.templateVariables;
+
+const env = nunjucks.configure({ tags: { variableStart: '${', variableEnd: '}' } });
+const replacedYamlContent = env.renderString(yaml.dump(parsedYaml), templateVariables);
+
+const parsedGlobalConfig = yaml.load(replacedYamlContent) as GlobalConfigTypes;
+
 const buildList = Object.keys(parsedGlobalConfig.dockerBuildConfig).filter(
   (key) => parsedGlobalConfig.dockerBuildConfig[key].endpointBuild,
 );
 
-// 根据 Dockerfile 路径生成新的构建配置对象
-const buildConfig =
-  generateBuildConfigTypesWithDockerfilePath(parsedGlobalConfig);
+console.log(parsedGlobalConfig.dockerBuildConfig.sdwebui_ext_build.serviceOptions.mountVolumes);
 
-// 根据构建配置对象生成项目选项数组
+const buildConfig = generateBuildConfigTypesWithDockerfilePath(parsedGlobalConfig);
+
 const projectOptions = Object.keys(buildConfig).map((key) => {
   const { label, hint, serviceOptions } = buildConfig[key];
   return {
@@ -32,7 +36,6 @@ const projectOptions = Object.keys(buildConfig).map((key) => {
   };
 });
 
-// 将 buildList 追加到 globalConfig 中
 export const globalConfig: GlobalConfigTypes = {
   ...parsedGlobalConfig,
   buildList: buildList,
